5ce0471bfd7e3bc2ebf9dde702dbbeb88a7e5858
NicolleMP <nimendespoltosi46@gmail.com>
2026-07-11 19:23:59 -0300

    feat(router): configura navegação das páginas com react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
-      <h1 className="text-2xl font-bold text-green-600">
-        EcoComunidade Iniciado!
-      </h1>
-    </div>
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<h1>Início</h1>} />
+        <Route path="/scanner" element={<h1>Scanner</h1>} />
+        <Route path="/diy" element={<h1>DIY</h1>} />
+        <Route path="/coleta" element={<h1>Coleta</h1>} />
+        <Route path="/about" element={<h1>Sobre</h1>} />
+      </Routes>
+    </>
   );
 }
 
