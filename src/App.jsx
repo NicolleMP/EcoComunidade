d00963afc3e70539523b1005ec030694c0bad252
NicolleMP <nimendespoltosi46@gmail.com>
2026-07-12 22:03:14 -0300

    feat: add separator line, organize page imports, and add the fonts to be used

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./pages/Home";
+import Scanner from "./pages/Scanner";
+import Diy from "./pages/Diy";
+import Coleta from "./pages/Coleta";
+import About from "./pages/About";
 
 function App() {
   return (
     <>
       <Header />
-
+      <hr className="border-none  h-px bg-[#ccc] m-0" />
       <Routes>
-        <Route path="/" element={<h1>Início</h1>} />
-        <Route path="/scanner" element={<h1>Scanner</h1>} />
-        <Route path="/diy" element={<h1>DIY</h1>} />
-        <Route path="/coleta" element={<h1>Coleta</h1>} />
-        <Route path="/about" element={<h1>Sobre</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/scanner" element={<Scanner />} />
+        <Route path="/diy" element={<Diy />} />
+        <Route path="/coleta" element={<Coleta />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </>
   );
