4e195c322e1a43aff1fea66a1a525223fe6d1702
NicolleMP <nimendespoltosi46@gmail.com>
2026-07-20 16:37:39 -0300

    refactor(router): isola configuração de rotas em um arquivo próprio

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
-import { Routes, Route } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
+
 import Header from "./components/Header";
-import Home from "./pages/Home";
-import Scanner from "./pages/Scanner";
-import Diy from "./pages/Diy";
-import Coleta from "./pages/Coleta";
-import About from "./pages/About";
 
 function App() {
   return (
     <>
       <Header />
       <hr className="border-none  h-px bg-[#ccc] m-0" />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/scanner" element={<Scanner />} />
-        <Route path="/diy" element={<Diy />} />
-        <Route path="/coleta" element={<Coleta />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <AppRoutes />
     </>
   );
 }
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Scanner from "../pages/Scanner";
+import Diy from "../pages/Diy";
+import Coleta from "../pages/Coleta";
+import About from "../pages/About";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/scanner" element={<Scanner />} />
+      <Route path="/diy" element={<Diy />} />
+      <Route path="/coleta" element={<Coleta />} />
+      <Route path="/aboute" element={<About />} />
+    </Routes>
+  );
+}
